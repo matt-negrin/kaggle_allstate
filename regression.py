66f2e2ebdffdbcce71bae877ec7521bcdd44211f
matt-negrin <matt.negrin@gmail.com>
2016-11-13 13:56:38 -0500

    added regression implementation and error columns

--- regression.py
+++ regression.py
@@ -25,29 +25,45 @@ def onehot_encoder(dataframe, column):
 for column in train.columns:
     if 'cat' in column:
         train = onehot_encoder(train, column)
-        
-# selecting columns to run regression
-total_rows = len(train.columns.format())
-starting_spot = np.where(train.columns=='cont1')[0][0]
-model_train = train.ix[:,starting_spot:total_rows]
 
 # building a training and validation set
 indexes = np.random.rand(len(train)) < 0.8
-model_train = train[indexes]
-model_valid = train[~indexes]
+idx_model_train = train[indexes]
+idx_model_valid = train[~indexes]
+        
+# selecting columns to run regression
+def column_selector(dataframe):
+    total_rows = len(dataframe.columns.format())
+    starting_spot = np.where(dataframe.columns=='cont1')[0][0]
+    output = dataframe.ix[:,starting_spot:total_rows]
+    return(output)
+
+model_train = column_selector(idx_model_train)
+model_valid = column_selector(idx_model_valid)
 
 # preping training and validation for regression
 from sklearn import linear_model
+
+# Split the data into training/testing sets
 x_train = model_train.drop('loss', axis=1)
 x_valid = model_valid.drop('loss', axis=1)
 
+# Split the targets into training/testing sets
 y_train = model_train['loss']
 y_valid = model_valid['loss']
 
-print('x_train: ', len(x_train))
-
 # Create linear regression object
-# regr = linear_model.LinearRegression()
+regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
-# regr.fit(diabetes_X_train, diabetes_y_train)
+regr.fit(x_train, y_train)
+
+# Creating copies of initial train and valid datasets
+output_train = pd.DataFrame.copy(idx_model_train)
+output_valid = pd.DataFrame.copy(idx_model_valid)
+
+# Adding predicted loss and error columns to initial datasets
+output_train['predicted_loss'] = regr.predict(x_train)
+output_valid['predicted_loss'] = regr.predict(x_valid)
+output_train['error'] = output_train['predicted_loss'] - output_train['loss']
+output_valid['error'] = output_valid['predicted_loss'] - output_valid['loss']
